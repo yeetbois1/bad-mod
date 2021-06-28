fd4c74100582c4074254efc1dafe462eb3b27692
aarush jain <aarushj2021@gmail.com>
2021-06-28 16:20:54 -0700

    created enderium shard and enderium ingot

---
+++ src/main/java/com/mr_sword/dragonarmor/registry/Enderium.java
@@ -0,0 +1,10 @@
+package com.mr_sword.dragonarmor.registry;
+
+import net.minecraft.item.Item;
+import net.minecraft.util.Rarity;
+
+public class Enderium {
+
+    public static final Item ENDERIUM_SHARD = new Item(new Item.Settings().group(RegsiterItems.DRAGON_ITEM_GROUP).rarity(Rarity.UNCOMMON));
+    public static final Item ENDERIUM_INGOT = new Item(new Item.Settings().group(RegsiterItems.DRAGON_ITEM_GROUP).rarity(Rarity.UNCOMMON));
+}
